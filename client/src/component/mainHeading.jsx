c1e2db4dd7bfc8355af741ea22a3479502c40989
Atharva Kadam <kadamatharva04@gmail.com>
2025-10-02 20:24:47 +0530

    Create mainHeading.jsx

---
+++ client/src/component/mainHeading.jsx
@@ -0,0 +1,11 @@
+import style from './mainHeading.module.css';
+
+function MainHeading() {
+    return (
+        <div className={style.mainHeadingg}>
+            <p className={style.mainn}>TradeLabX</p>
+                <p className={style.edgee}>The Edge behind every Trade</p>
+        </div>
+    )
+}
+export default MainHeading;
