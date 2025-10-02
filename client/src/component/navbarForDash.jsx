2bd2cb8fc3fa96127e5dfdeb1793257b9cebb4eb
Atharva Kadam <kadamatharva04@gmail.com>
2025-10-02 20:23:17 +0530

    Create navbarForDash.jsx

---
+++ client/src/component/navbarForDash.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import style from './navbarForDash.module.css';
+import { useNavigate } from 'react-router'
+import { Link } from 'react-router'
+import TradingViewWidget from "../tradingview";
+
+
+function NavbarForDash() {
+    const navigate = useNavigate();
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    return (
+        <div className={style.navbar}>
+            <div className={style.logo}>
+                <h2>TradeLabX</h2>
+            </div>
+            <div className={style.tradingview}>
+                <TradingViewWidget />
+            </div>
+            <ul className={style.optionEdit}>
+                <li><Link to={'/dashboard'} className={style.link}>Home</Link></li>
+                <li><Link to={'/history'} className={style.link}>History</Link></li>
+                <li className={style.logout}><button onClick={() => navigate('/login')}>Logout</button></li>
+                <li><button className={style.profileButton}><Link to='/userProfile'><img src={user.profileImg} alt="" /></Link></button></li>
+            </ul>
+        </div>
+    )
+}
+export default NavbarForDash;
